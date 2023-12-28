ad47b9aec0ced7158f193acf271375adad2ffb41
Sriram <sriram@Srirams-MBP.lan>
2023-12-27 23:40:01 -0600

    first commit

---
+++ src/main/java/com/sriram/home/finance/database/scripts/ShfDatabaseScriptsApplication.java
@@ -0,0 +1,13 @@
+package com.sriram.home.finance.database.scripts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShfDatabaseScriptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShfDatabaseScriptsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sriram/home/finance/database/scripts/ShfDatabaseScriptsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sriram.home.finance.database.scripts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShfDatabaseScriptsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
